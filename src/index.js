5fb95cf4a12944dc86fd86dee4b379293806bfb3
MohamedOsoble <Mohamed.Osoble@outlook.com>
2024-08-14 21:40:26 +0100

    update some style and add api values

--- src/index.js
+++ src/index.js
@@ -2,18 +2,30 @@
 import './style.css';
 
 // Set up API website and API Key
-
 const APIKEY = "HT5ZKT8RYEGF3V64XZFXSYZ75"
 const APISITESTART = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
 const APISITEEND = "?unitGroup=metric&include=events%2Cdays%2Ccurrent%2Chours%2Calerts&key=" + APIKEY + "&contentType=json"
 
-// Initialize HTML elements to update with weather
+// Initialize °C const
+const DEGREE = "°C"
 
-let currCitylement = document.getElementById('current-city');
-let currentTempElement = document.getElementById('temp-current-value');
-let maxTempElement = document.getElementById('temp-max-value');
-let minTempElement = document.getElementById('temp-min-value');
-let weatherImageElement = document.getElementById('weather-image')
+// Initialize Current location and Date elements
+const currCity = document.getElementById('curr-location');
+const currDate = document.getElementById('curr-date');
+
+// Initialize current weather elements
+const currTemp = document.getElementById('curr-temp');
+const currMinMax = document.getElementById('curr-min-max');
+const feelsLikeTemp = document.getElementById('curr-feels-like');
+const currWeatherImg = document.getElementById('curr-weather-img');
+const currWeatherDesc = document.getElementById('curr-weather-desc');
+const currFeelsLike = document.getElementById('curr-feels-like');
+
+// Initialize weekly forecast elements
+// To be entered dynamically later
+
+//Initialize button to handle updating location/temp
+//document.getElementById('search-button').addEventListener("click", console.log(document.getElementById('location-search').value))
 
 function queryBuilder(location){
     return APISITESTART + location + APISITEEND
@@ -35,15 +47,38 @@ function fetchData(location){
 };
 
 function processData(data){
-    currCitylement.innerHTML = "Location: " + data.address;
-    currentTempElement.innerHTML = data.currentConditions.temp;
-    maxTempElement.innerHTML = data.days[0].tempmax
-    minTempElement.innerHTML = data.days[0].tempmin
     console.log(data)
+    currCity.textContent = data.resolvedAddress;
+    currDate.textContent = getDate() + " " + data.currentConditions.datetime;
+    currTemp.textContent = data.currentConditions.temp + DEGREE;
+    currWeatherImg.src = getIcon(data.currentConditions.icon)
+    currWeatherImg.alt = data.currentConditions.conditions + ' icon';
+    currWeatherDesc.textContent = data.currentConditions.conditions;
+    currFeelsLike.textContent = "Feels like: " + data.currentConditions.feelslike + DEGREE
+    currMinMax.textContent = "Min Today: " + data.days[0].tempmin + DEGREE + " | Max Today: " + data.days[0].tempmax + DEGREE;
+    // currMinMax.innerHTML = data.days[0].tempmax
+    // feelsLikeTemp.innerHTML = data.days[0].tempmin
+    // console.log(data)
 };
 
 function processInput(location){
     fetchData(location)
+};
+
+function getDate(){
+    let timeElapsed = Date.now();
+    let today = new Date(timeElapsed);
+    const options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+      };
+    return today.toLocaleString('en-GB', options);
+}
+
+function getIcon(iconName){
+    return './icons/' + iconName.toString() + '.png';
 };
 
 processInput("London");
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
@@ -11,6 +12,10 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
       },
     ],
   },
